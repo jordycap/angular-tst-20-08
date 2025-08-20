c7a8c3be40f764ffa38570346a1a3e6f40f2f15e
Jordy van Altena <jordy.van.altena@capgemini.com>
2025-08-20 16:42:51 +0200

    rm karma and add test:ci

--- karma.conf.js
+++
@@ -1,8 +0,0 @@
-// karma.conf.js
-module.exports = function (config) {
-  config.set({
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    browsers: ['ChromeHeadless'], // run headless Chrome
-    singleRun: true               // exit after running tests
-  });
-};
